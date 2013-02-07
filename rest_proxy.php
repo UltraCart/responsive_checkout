83ef1365dd094903333fb672a17e65c619d94bc8
Perry Tew <perry@ultracart.com>
2013-02-07 16:32:22 -0500

    rest_proxy.php wasn't passing along GET parms.

--- rest_proxy.php
+++ rest_proxy.php
@@ -1,21 +1,33 @@
 <?php
+// Version 0.5.  02/07/2013  Initial Version.
+
 if(isset($_GET["_url"])){
   $path = $_GET["_url"]; // get the url parameter
 } else {
   die("UltraCart rest proxy script called incorrectly.  _url query parameter is required.");
 }
 
+
 $path = preg_replace('#[^a-z0-9/]#i', '', $path); // strip off any junk
 $path = preg_replace('#/+#', '/', $path); // remove duplicate slashes if any
-if(!strncmp($path, '/', 1)){ // if the path doesn't start with a slash, add one.
+if(strncmp($path, '/', 1) != 0){ // if the path doesn't start with a slash, add one.
   $path = '/' . $path;
 }
 
-// the above filtering should remove any malicious attempts, but no worries, UltraCart has some insane firewalls to boot.
-$server_get_url = "https://secure.ultracart.com" . $path;
-$post_data = file_get_contents('php://input');
+$additional_parameters = '';
+foreach( $_GET as $k=>$v ){
+  if($k != '_url'){
+    if($additional_parameters){
+      $additional_parameters = $additional_parameters . '&' . $k . "=" . urlencode($v);
+    } else {
+      $additional_parameters = $additional_parameters . '?' . $k . "=" . urlencode($v);
+    }
+  }
+}
 
-// error_log($post_data);
+// the above filtering should remove any malicious attempts, but no worries, UltraCart has some insane firewalls to boot.
+$server_get_url = "https://secure.ultracart.com" . $path . $additional_parameters;
+$post_data = file_get_contents('php://input');
 
 foreach($_SERVER as $i=>$val) {
 // error_log($i);
